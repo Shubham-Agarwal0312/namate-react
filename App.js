fdb9baadd77d702e9506ce686c3c7ea590a85f45
Shubham-Agarwal0312 <shubhamagarwal0312@gmail.com>
2023-12-25 15:23:30 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,5 @@
+// import './index.css';
+
+const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
